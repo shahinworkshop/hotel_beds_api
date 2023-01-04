36b84f1f12d607555c0fd63ff31ad31ce1c4556e
Shahin Ataei <shahin.ataei@instapro.com>
2023-01-04 22:31:40 +0100

    Initial structure
    Create the structure from Symfony skeleton

---
+++ app/tests/Unit/HealthTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class HealthTest extends TestCase
+{
+    public function testHealth(): void
+    {
+        $this->assertTrue(true);
+    }
+}
